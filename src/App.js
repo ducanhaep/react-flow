8059759372d2dee3339cf480e3750c633eb28ac7
ducanhaep <tranducanh1012@gmail.com>
2019-03-07 09:14:45 +0700

    save code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+type TestProps = {
+  str: string,
+  num?: number,
+  arr:Array<number>,
+};
+
+type TestState ={
+  message: number,
+}
+type FuncProp = {
+  cc: number,
+}
+const FuncComp = (props: FuncProp) => {
+  return(
+    <div>{props.cc}</div>
+  )
+}
+
+class Test extends Component<TestProps,TestState> {
+
+  state = {
+    message: 1,
+  }
+  static defaultProps = {
+    str: "gfdjl",
+
+  }
+  h1:?HTMLHeadingElement;
+  render() {
+    return (
+      <div>
+        <h1>{this.props.str}</h1>
+        <h1>{this.props.num}</h1>
+        <h1>{this.props.arr.map(item=>{
+          return(<li>{item}</li>)
+        })}</h1>
+        <h1 ref={el => this.h1 = el}>{this.state.message}</h1>
+      </div>
+    );
+  }
+}
+
+class App extends Component<{}> {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Test
+          arr={[1,2,3]}
+        >
+
+        </Test>
+        <FuncComp cc={1}>
+        </FuncComp>
       </div>
     );
   }
